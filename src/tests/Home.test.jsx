07f47e63f8bc6b1722fd332836cc9555d3d095df
Rafael Ribeiro <rafittu@outlook.com>
2022-05-02 17:11:13 -0300

    test: todo page is rendering

---
+++ src/tests/Home.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('Testing home page', () => {
+  render(<App />);
+  it('Todo page is rendering', () => {
+    const todoTitle = screen.getByText(/todo list/i);
+    expect(todoTitle).toBeInTheDocument();
+  });
+});
