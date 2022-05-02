b179a85c1b0f0ede17af23039b45f37133d4cb5e
Rafael Ribeiro <rafittu@outlook.com>
2022-05-02 16:11:19 -0300

    feat: add route to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <h1>TODO LIST</h1>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
