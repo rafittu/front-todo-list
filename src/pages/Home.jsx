3a5c4b9ba67174cf85ad03fd1b83f0f3ec656d2b
Rafael Ribeiro <rafittu@outlook.com>
2022-05-02 16:35:21 -0300

    feat: implement map function to list users

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import * as requests from '../services/requests';
+import User from '../components/User';
 
 function Home() {
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState();
 
   useEffect(() => {
     const getUsers = async () => {
       const { response, data } = await requests.getUsers();
       if (response.status !== 200) return response;
-      return setUsers(data);
+      setUsers(data);
+
+      return setIsLoading('false');
     };
     getUsers();
-  });
-
-  console.log(users);
+  }, []);
 
   return (
-    <h1>TODO LIST</h1>
+    <div>
+      <h1>TODO LIST</h1>
+
+      <span>
+        {
+          !isLoading
+            ? 'Carregando...'
+            : users.map((user) => (
+              <User id={user.id} key={user.id} name={user.name} />
+            ))
+        }
+      </span>
+
+    </div>
   );
 }
 
