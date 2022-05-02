4302a14f4878eb6ad5c64c7bd5c0b5469eec3a00
Rafael Ribeiro <rafittu@outlook.com>
2022-05-02 18:25:01 -0300

    feat: implement add todo input

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+function AddTask({ addTodo }) {
+  const [newTask, setNewTask] = useState('');
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { value } = e.target;
+    setNewTask(value);
+  };
+
+  const submitTodo = () => {
+    addTodo(newTask);
+    setNewTask('');
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          placeholder="Add a todo"
+          name="task"
+          value={newTask}
+          onChange={handleChange}
+        />
+        <button type="button" onClick={submitTodo}>Add task</button>
+        <hr />
+      </form>
+    </div>
+  );
+}
+
+AddTask.propTypes = {
+  addTodo: PropTypes.func,
+}.isRequired;
+
+export default AddTask;
