a93188c4695f62776ab7d1bb8ec0d8de80b9c2ee
Rafael Ribeiro <rafittu@outlook.com>
2022-05-03 16:15:37 -0300

    feat: add input to check task as completed

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Tasks({ id, title, deleteTodo }) {
+function Tasks({
+  id, title, updateTodo, deleteTodo,
+}) {
+  const handleComplete = () => {
+    updateTodo(id);
+  };
+
   const handleDelete = () => {
-    deleteTodo(Number(id));
+    deleteTodo(id);
   };
 
   return (
     <li id={id}>
+      <input type="checkbox" onClick={handleComplete} />
       {title}
       <button type="button" onClick={handleDelete}>delete</button>
     </li>
@@ -17,6 +24,7 @@ function Tasks({ id, title, deleteTodo }) {
 Tasks.propTypes = {
   id: PropTypes.number,
   title: PropTypes.string,
+  completeTask: PropTypes.func,
   deleteTodo: PropTypes.func,
 }.isRequired;
 
